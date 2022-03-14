82192c465b99f5766d9963e27aac4bf2722ea51e
thijsgelton <thijs.gelton@hotmail.com>
2022-03-14 10:00:48 +0100

    Clean up

--- nnunet/training/loss_functions/dice_loss.py
+++ nnunet/training/loss_functions/dice_loss.py
@@ -11,7 +11,7 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-
+from typing import List
 
 import torch
 from torchvision.transforms.functional import to_tensor
@@ -464,54 +464,18 @@ class DC_and_topk_loss(nn.Module):
         return result
 
 
-class DC_and_CE_loss_weighted(DC_and_CE_loss):
+class DCandCELossWeighted(DC_and_CE_loss):
 
-    def __init__(self, class_weights, soft_dice_kwargs, ce_kwargs):
+    def __init__(self, class_weights: List[float], soft_dice_kwargs, ce_kwargs):
         """
-        CAREFUL. Weights for CE and Dice do not need to sum to one. You can set whatever you want.
-        :param soft_dice_kwargs:
-        :param ce_kwargs:
-        :param aggregate:
-        :param square_dice:
-        :param weight_ce:
-        :param weight_dice:
+        Weighted version of the DC and CE loss combination.
         """
-        super(DC_and_CE_loss_weighted, self).__init__(soft_dice_kwargs, ce_kwargs)
+        super(DCandCELossWeighted, self).__init__(soft_dice_kwargs, ce_kwargs)
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.class_weights = self.to_tensor(np.array(class_weights))
         self.dc = SoftDiceLossWeighted(self.class_weights, apply_nonlin=softmax_helper,
                                        soft_dice_kwargs=soft_dice_kwargs)
         self.ce = RobustCrossEntropyLoss(weight=self.class_weights, **ce_kwargs)
-
-    def forward(self, net_output, target):
-        """
-        target must be b, c, x, y(, z) with c=1
-        :param net_output:
-        :param target:
-        :return:
-        """
-        if self.ignore_label is not None:
-            assert target.shape[1] == 1, 'not implemented for one hot encoding'
-            mask = target != self.ignore_label
-            target[~mask] = 0
-            mask = mask.float()
-        else:
-            mask = None
-
-        dc_loss = self.dc(net_output, target, loss_mask=mask) if self.weight_dice != 0 else 0
-        if self.log_dice:
-            dc_loss = -torch.log(-dc_loss)
-
-        ce_loss = self.ce(net_output, target[:, 0].long()) if self.weight_ce != 0 else 0
-        if self.ignore_label is not None:
-            ce_loss *= mask[:, 0]
-            ce_loss = ce_loss.sum() / mask.sum()
-
-        if self.aggregate == "sum":
-            result = self.weight_ce * ce_loss + self.weight_dice * dc_loss
-        else:
-            raise NotImplementedError("nah son")  # reserved for other stuff (later)
-        return result
 
     def to_tensor(self, array):
         return torch.tensor(array, dtype=torch.float32, device=self.device)
