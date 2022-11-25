13ee8ac79cd9dcce6d6809c9619a2316496cc9b4
JoeySpronck <joeyspronck67@hotmail.com>
2022-11-25 18:57:23 +0100

    changed experiment planner identifiers

--- nnunet/experiment_planning/alternative_experiment_planning/normalization/experiment_planner_2DUNet_v21_RGB_scaleto_0_1_bs8_ps512.py
+++ nnunet/experiment_planning/alternative_experiment_planning/normalization/experiment_planner_2DUNet_v21_RGB_scaleto_0_1_bs8_ps512.py
@@ -23,7 +23,7 @@ class ExperimentPlanner2D_v21_RGB_scaleTo_0_1_bs8_ps512(ExperimentPlanner2D_v21)
     """
     def __init__(self, folder_with_cropped_data, preprocessed_output_folder):
         super().__init__(folder_with_cropped_data, preprocessed_output_folder)
-        self.data_identifier = "nnUNet_RGB_scaleTo_0_1"
+        self.data_identifier = "nnUNet_RGB_scaleTo_0_1_bs8_ps512"
         self.plans_fname = join(self.preprocessed_output_folder, "nnUNet_RGB_scaleTo_0_1" + "_plans_2D.pkl")
 
         # The custom preprocessor class we intend to use is GenericPreprocessor_scale_uint8_to_0_1. It must be located
