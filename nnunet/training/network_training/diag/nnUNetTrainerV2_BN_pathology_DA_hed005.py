798f2ca4f61c145c2460241aa18b3f67e22eef53
JoeySpronck <joeyspronck67@hotmail.com>
2022-12-25 11:56:14 +0100

    corrected parent trainer class import

--- nnunet/training/network_training/diag/nnUNetTrainerV2_BN_pathology_DA_hed005.py
+++ nnunet/training/network_training/diag/nnUNetTrainerV2_BN_pathology_DA_hed005.py
@@ -34,7 +34,7 @@ from torch import nn
 from torch.cuda.amp import autocast
 from nnunet.training.learning_rate.poly_lr import poly_lr
 from batchgenerators.utilities.file_and_folder_operations import *
-
+from nnunet.training.network_training.diag import nnUNetTrainerV2_BN_pathology_DA
 
 class nnUNetTrainerV2_BN_pathology_DA_hed005(nnUNetTrainerV2_BN_pathology_DA):
     """
